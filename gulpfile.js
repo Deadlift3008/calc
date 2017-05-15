7a87e4c0ca03afbc127d591a7b4dfaf8b039321f
Deadlift <Deadlift3008@gmail.com>
2017-05-15 13:13:23 +0300

    set environment

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    babel = require('gulp-babel'),
+    babelify = require('babelify'),
+    browserify = require('browserify'),
+    buffer = require('vinyl-buffer'),
+    source = require('vinyl-source-stream');
+
+
+gulp.task("js",function(){
+    return browserify('./index.js')
+    .transform('babelify', {
+        presets: ['es2015', 'react'],
+        plugins: ['transform-object-rest-spread']
+    })
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest('build'));
+});
+
+
+
+
+gulp.task("watch", function(){
+    gulp.watch("js/*.js",['js']);
+});
+
+gulp.task("build",["js"]);
